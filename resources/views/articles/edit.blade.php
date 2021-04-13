abd9cb9d2980a06ca4a53304b202b5f77c745a06
ilhamibrahim-id <ii.ilhamibrahim@gmail.com>
2021-04-13 15:23:49 +0700

    memperbaiki view edit yang salah

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -12,16 +12,13 @@ required="required" name="title" value="{{$article->title}}"></br>
  <div class="form-group">
  <label for="content">Content</label>
  <input type="text" class="form-control"
-required="required" name="content" value="{{$article-
->content}}"></br>
+required="required" name="content" value="{{$article->content}}"></br>
  </div>
  <div class="form-group">
  <label for="image">Feature Image</label>
  <input type="file" class="form-control"
-required="required" name="image" value="{{$article-
->featured_image}}"></br>
- <img width="150px" src="{{asset('storage/'.$article-
->featured_image)}}">
+required="required" name="image" value="{{$article->featured_image}}"></br>
+ <img width="150px" src="{{asset('storage/'.$article->featured_image)}}">
  </div>
  <button type="submit" class="btn btn-primary float-right">Ubah
 Data</button>
