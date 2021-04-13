b3539585628cc3d58901435fccb28d4bf16ab059
ilhamibrahim-id <ii.ilhamibrahim@gmail.com>
2021-04-13 16:39:45 +0700

    "Tugas - Pembuatan Model Mahasiswa"

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Models;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //Model Eloquent
+use App\Models\Kelas;
+class Mahasiswa extends Model //Definisi Model
+{
+    protected $table="mahasiswa"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    protected $primaryKey = 'nim';
+ /**
+ * The attributes that are mass assignable.
+ *
+ * @var array
+ */
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_handphone',
+        'email',
+        'tanggal_lahir'
+    ];
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+    public function matakuliah()
+    {
+        return $this->belongsToMany(MataKuliah::class, 'mahasiswa_matakuliah', 'mahasiswa_nim', 'matakuliah_id')->withPivot('nilai');
+    }
+};
